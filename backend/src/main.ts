a85d54174f0bd84917ec2cab9967688013788e9e
yuschyky <yuriyyu@wizardsdev.com>
2022-11-19 18:43:39 +0200

    feature: init project

---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.setGlobalPrefix("api");
+
+  const config = new DocumentBuilder()
+    .setTitle("Durak Roll API")
+    .setVersion("1.0")
+    .addBearerAuth({ type: "http", scheme: "bearer", bearerFormat: "JWT" })
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("swagger", app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
